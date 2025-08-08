a78a6564e71c2ac4feab4bcde12302d2df77ab8c
ChancellorIkseew <ikse.ksini@bk.ru>
2025-08-08 13:22:14 +1000

    grammar fix

--- src/config.hpp
+++ src/config.hpp
@@ -1,12 +1,12 @@
 #pragma once
 //
 #if defined(_MSC_VER)
-///@brief force inine macro
+///@brief force inline macro
 #define t1_finline __forceinline
 #elif defined(__GNUC__) || defined(__clang__)
-///@brief force inine macro
+///@brief force inline macro
 #define t1_finline __attribute__((always_inline)) inline
 #else
-///@brief force inine macro
+///@brief force inline macro
 #define t1_finline inline
 #endif
