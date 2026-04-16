1b4ec8192e05fce887485360bac49acbdec0f326
ChancellorIkseew <ikse.ksini@bk.ru>
2026-04-16 13:23:15 +1000

    update util time

--- src/engine/util/time.cpp
+++ src/engine/util/time.cpp
@@ -1,42 +1,32 @@
 #include "time.hpp"
 //
 #include <chrono>
-#include <ctime>
-#include <sstream>
-#pragma warning(disable : 4996)
+
+using namespace std::chrono;
 
 uint64_t util::time::getLocalTimeSeconds() noexcept {
-    using namespace std::chrono;
-    auto sec = duration_cast<seconds>(system_clock::now().time_since_epoch()).count();
+    const auto sec = duration_cast<seconds>(system_clock::now().time_since_epoch()).count();
     return static_cast<uint64_t>(sec);
 }
 
 uint64_t util::time::getLocalTimeMilliseconds() noexcept {
-    using namespace std::chrono;
-    auto ms = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+    const auto ms = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
     return static_cast<uint64_t>(ms);
 }
 
 std::string util::time::yyyyMMddHHmmSS(const uint64_t seconds) {
-    time_t tm = static_cast<time_t>(seconds);
-    std::stringstream ss;
-    ss << std::put_time(std::localtime(&tm), "%Y/%m/%d %T");
-    return ss.str();
+    const sys_seconds timePoint{ std::chrono::seconds(seconds) };
+    const local_seconds local = std::chrono::current_zone()->to_local(timePoint);
+    return std::format("{:%Y/%m/%d %T}", local);
 }
 
 std::string util::time::timerFormat(const uint64_t seconds) {
     const uint64_t hours = seconds / 3600;
     const uint64_t minutes = (seconds / 60) % 60;
     const uint64_t secondsOnly = seconds % 60;
-    std::stringstream ss;
-    //
-    if (hours > 0)/*------------*/ss << hours << ':';
-    //
-    if (hours > 0)/*------------*/ss << std::setfill('0') << std::setw(2) << minutes << ':';
-    else if (minutes > 0)/*-----*/ss << minutes << ':';
-    //
-    if (hours > 0 || minutes > 0) ss << std::setfill('0') << std::setw(2) << secondsOnly;
-    else/*----------------------*/ss << secondsOnly;
-    //
-    return ss.str();
+    if (hours > 0)
+        return std::format("{}:{:02}:{:02}", hours, minutes, secondsOnly);
+    if (minutes > 0)
+        return std::format("{}:{:02}", minutes, secondsOnly);
+    return std::format("{}", secondsOnly);
 }
