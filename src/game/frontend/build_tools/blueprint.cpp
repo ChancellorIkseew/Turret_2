f07b3f85840edd07c8d006bcc1222c1434ee0127
ChancellorIkseew <ikse.ksini@bk.ru>
2026-06-03 16:21:19 +1000

    add turret drawing for blueprints

--- src/game/frontend/build_tools/blueprint.cpp
+++ src/game/frontend/build_tools/blueprint.cpp
@@ -4,12 +4,32 @@
 #include "engine/engine.hpp"
 #include "engine/render/renderer.hpp"
 
+static void drawBlock(const Presets& presets, const Renderer& renderer, const TileCoord tile, const BlockPresetID presetID, const BlockRot rotation) {
+    const BlockPreset& preset = presets.getBlock(presetID);
+    const PixelCoord size = preset.visual.size;
+    const PixelCoord position = t1::pixel(tile);
+
+    if (!preset.rotatable)
+        renderer.drawFast(preset.visual.texture, position, size);
+    else {
+        const float angleRad = static_cast<float>(rotation) * t1::PI_F / 2.f;
+        const PixelCoord origin = size / 2.f;
+        renderer.draw(preset.visual.texture, position + origin, size, origin, angleRad);
+        if (preset.archetype == BlockType::turret) {
+            const TurretPreset& turret = presets.getTurret(preset.turret);
+            const PixelCoord blockCenter = position + origin;
+            renderer.draw(turret.visual.texture, blockCenter, turret.visual.size, turret.visual.origin, angleRad + t1::PI_F);
+        }
+    }
+}
+
 void Blueprints::draw(Renderer& renderer, const Engine& engine) const {
     const float timeMs = static_cast<float>(engine.getMainWindow().getTime());
     const float modifier = std::sin(timeMs / 500.f) * 64.f;
     const uint8_t alpha = uint8_t(modifier) + 191; // 255 - 64
     renderer.setColorModifier(255, 255, 255, alpha);
     for (const auto& blueprint : blueprints) {
+        const Presets& presets = engine.getAssets().getPresets();
         const BlockPreset& preset = engine.getAssets().getPresets().getBlock(blueprint.presetID);
         const PixelCoord position = t1::pixel(blueprint.tile);
         const PixelCoord size = preset.visual.size;
@@ -18,13 +38,7 @@ void Blueprints::draw(Renderer& renderer, const Engine& engine) const {
             renderer.drawRect(0x00'FF'00'FF, position - OFFSET, size + OFFSET * 2.f);
             //TODO: update after renderer reimplementation
         }
-        if (!preset.rotatable)
-            renderer.drawFast(preset.visual.texture, position, size);
-        else {
-            const float angleRad = static_cast<float>(blueprint.rotation) * t1::PI_F / 2.f;
-            const PixelCoord origin = size / 2.f;
-            renderer.draw(preset.visual.texture, position + origin, size, origin, angleRad);
-        }
+        drawBlock(presets, renderer, blueprint.tile, blueprint.presetID, blueprint.rotation);
     }
     renderer.resetColorModifier();
 }
