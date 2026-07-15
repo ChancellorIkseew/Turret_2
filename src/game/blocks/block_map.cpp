05754df4aacab62bb2c8ea94d46c49829c120206
ChancellorIkseew <ikse.ksini@bk.ru>
2026-07-16 02:39:13 +1000

    move BlockMap place and demolish to cpp

--- src/game/blocks/block_map.cpp
+++ src/game/blocks/block_map.cpp
@@ -4,6 +4,31 @@
 #include "engine/assets/presets.hpp"
 #include "make_block.hpp"
 
+void BlockMap::place(TileCoord tile, TeamID teamID, std::unique_ptr<Block>& block) {
+    demolish(tile);
+    if (block->getType() == BlockType::turret) {
+        TurretBlock* turretBlock = static_cast<TurretBlock*>(block.get());
+        TurretPresetID preset = turretBlock->turretPreset;
+        const AngleRad angleRad = static_cast<float>(turretBlock->defaultRotation) * t1::TAU;
+        meta.getTurrets().addTurret(preset, t1::tileCenter(tile), t1::PI - angleRad, teamID, ShootingData(), 0);
+    }
+    if (block->getType() == BlockType::core)
+        meta.setCorePosition(tile);
+    if (block->getType() == BlockType::in_progress)
+        meta.addBlockInProgress(tile);
+    at(tile).place(teamID, block);
+}
+
+void BlockMap::demolish(TileCoord tile) {
+    if (at(tile).type == BlockType::turret)
+        meta.markForRemove(tile);
+    if (at(tile).type == BlockType::core)
+        meta.setCorePosition(std::nullopt);
+    if (at(tile).type == BlockType::in_progress)
+        meta.removeBlockInProgress(tile);
+    at(tile).demolish();
+}
+
 void BlockMap::build(const TileCoord tile, const TeamID teamID, const int8_t buildSpeed, const Presets& presets) {
     assert(dynamic_cast<InProgress*>(at(tile).block.get()) && isInProgress(tile));
     //
--- src/game/blocks/block_map.hpp
+++ src/game/blocks/block_map.hpp
@@ -106,29 +106,8 @@ public:
     bool isAir(TileCoord tile)    const noexcept { return contains(tile) && at(tile).type == BlockType::air; }
     bool isInProgress(TileCoord tile) const noexcept { return contains(tile) && at(tile).type == BlockType::in_progress; }
     //
-    void place(TileCoord tile, TeamID teamID, std::unique_ptr<Block>& block) {
-        demolish(tile);
-        if (block->getType() == BlockType::turret) {
-            TurretBlock* turretBlock = static_cast<TurretBlock*>(block.get());
-            TurretPresetID preset = turretBlock->turretPreset;
-            const AngleRad angleRad = static_cast<float>(turretBlock->defaultRotation) * t1::TAU;
-            meta.getTurrets().addTurret(preset, t1::tileCenter(tile), t1::PI - angleRad, teamID, ShootingData(), 0);
-        }
-        if (block->getType() == BlockType::core)
-            meta.setCorePosition(tile);
-        if (block->getType() == BlockType::in_progress)
-            meta.addBlockInProgress(tile);
-        at(tile).place(teamID, block);
-    }
-    void demolish(TileCoord tile) {
-        if (at(tile).type == BlockType::turret)
-            meta.markForRemove(tile);
-        if (at(tile).type == BlockType::core)
-            meta.setCorePosition(std::nullopt);
-        if (at(tile).type == BlockType::in_progress)
-            meta.removeBlockInProgress(tile);
-        at(tile).demolish();
-    }
+    void place(TileCoord tile, TeamID teamID, std::unique_ptr<Block>& block);
+    void demolish(TileCoord tile);
     void build(const TileCoord tile, const TeamID teamID, const int8_t buildSpeed, const Presets& presets);
 
     t1_disable_copy_and_move(BlockMap)
