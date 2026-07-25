6a606c3bc4fa7ac24e9b8fb617067f317da8b4bf
ChancellorIkseew <ikse.ksini@bk.ru>
2026-07-25 21:48:04 +1000

    reduce blueprint frame size

--- src/game/blocks/schematic/schematic.cpp
+++ src/game/blocks/schematic/schematic.cpp
@@ -8,20 +8,20 @@ void Schematic::drawBlockFrame(Renderer& renderer, const TileCoord tile, const i
     const PixelCoord position = t1::pixel(tile);
     const float pixelSize = t1::pixelF(size);
     //
-    renderer.drawRect(position, PixelCoord(12, 4), PixelCoord(0, 0), 0.f, color);
-    renderer.drawRect(position, PixelCoord(4, 12), PixelCoord(0, 0), 0.f, color);
+    renderer.drawRect(position, PixelCoord(10, 4), PixelCoord(0, 0), 0.f, color);
+    renderer.drawRect(position, PixelCoord(4, 10), PixelCoord(0, 0), 0.f, color);
     //
     const PixelCoord topRight(position.x + pixelSize, position.y);
-    renderer.drawRect(topRight, PixelCoord(12, 4), PixelCoord(12, 0), 0.f, color);
-    renderer.drawRect(topRight, PixelCoord(4, 12), PixelCoord(4, 0), 0.f, color);
+    renderer.drawRect(topRight, PixelCoord(10, 4), PixelCoord(10, 0), 0.f, color);
+    renderer.drawRect(topRight, PixelCoord(4, 10), PixelCoord(4, 0), 0.f, color);
     //
     const PixelCoord bottomLeft(position.x, position.y + pixelSize);
-    renderer.drawRect(bottomLeft, PixelCoord(12, 4), PixelCoord(0, 4), 0.f, color);
-    renderer.drawRect(bottomLeft, PixelCoord(4, 12), PixelCoord(0, 12), 0.f, color);
+    renderer.drawRect(bottomLeft, PixelCoord(10, 4), PixelCoord(0, 4), 0.f, color);
+    renderer.drawRect(bottomLeft, PixelCoord(4, 10), PixelCoord(0, 10), 0.f, color);
     //
     const PixelCoord bottomRight(position.x + pixelSize, position.y + pixelSize);
-    renderer.drawRect(bottomRight, PixelCoord(12, 4), PixelCoord(12, 4), 0.f, color);
-    renderer.drawRect(bottomRight, PixelCoord(4, 12), PixelCoord(4, 12), 0.f, color);
+    renderer.drawRect(bottomRight, PixelCoord(10, 4), PixelCoord(10, 4), 0.f, color);
+    renderer.drawRect(bottomRight, PixelCoord(4, 10), PixelCoord(4, 10), 0.f, color);
 }
 
 void Schematic::drawRange(Renderer& renderer, const PixelCoord center, const float range) {
