d8158101ad8039d8c28cf447be357a78998b516b
ChancellorIkseew <ikse.ksini@bk.ru>
2026-08-15 20:06:15 +1000

    fix logic error

--- src/game/systems/construction_system.cpp
+++ src/game/systems/construction_system.cpp
@@ -30,7 +30,7 @@ void construction::buildBlueprints(MobSoA& soa, const Presets& presets, Schemati
     BlockMap& blocks, BuildBeamsPool& buildBeams, TeamsPool& teams, SoundQueue& sounds, ParticlesPool& particles) {
     for (size_t i = 0; i < soa.mobCount; ++i) {
         const auto& mobPreset = presets.getMob(soa.preset[i]);
-        if (!mobPreset.buildSpeed > 0)
+        if (mobPreset.buildSpeed < 1)
             continue;
 
         const PixelCoord position = soa.position[i];
