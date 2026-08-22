fa9df6322b749112ddb4fbef32a655280c627aa5
ChancellorIkseew <ikse.ksini@bk.ru>
2026-08-22 22:31:55 +1000

    change version

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char* argv[]) {
     try {
         Settings::readSettings();
         Controls::readBindings();
-        Engine engine("Turret_2.0.14 - pre-alpha", PixelCoord(720, 480));
+        Engine engine("Turret_2.0.15 - pre-alpha", PixelCoord(720, 480));
         Settings::applySettings(engine);
         engine.run();
     }
