d1099ef406480aa2c412dccbdc70312ec8ffa0ea
ChancellorIkseew <ikse.ksini@bk.ru>
2026-09-15 22:42:56 +1000

    delete comments in smoke shader

--- res/shaders/fragment_smoke.glsl
+++ res/shaders/fragment_smoke.glsl
@@ -1,19 +1,13 @@
 #version 460 core
-
+//
 layout (location = 0) in vec2 outLocalPos; // [-1, 1]
 layout (location = 1) in vec4 inColor;     // RGB = color, A = intensity
-
+//
 layout (location = 0) out vec4 fragColor;
 
 void main() {
     float distance = length(outLocalPos);
-    
-    // Используем ровно тот же smoothstep, что и в шейдере освещения
     float edgeSmoothing = smoothstep(1.0, 0.0, distance);
-    
-    // Вся интенсивность затухания идет в альфа-канал (прозрачность)
     float finalAlpha = inColor.a * edgeSmoothing;
-    
-    // fragColor выводит цвет (inColor.rgb) и альфа-прозрачность
     fragColor = vec4(inColor.rgb, finalAlpha);
 }
